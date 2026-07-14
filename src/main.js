6f1209f82c7d94560cee633a77598b70b8be3a50
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2026-07-14 18:23:36 +0100

    fix: import CSS via JS entry; remove manual stylesheet link; set vite base

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { on, start } from "./router";
 import home from "./pages/home";
 import project from "./pages/project";
+import "./styles/main.css";
 
 on("/", home);
 on("project/:name", project);
